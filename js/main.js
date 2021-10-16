dab38c48b12ed55e328c407e18384150f5a27fa1
KojimaShunki <90170219+KojimaShunki@users.noreply.github.com>
2021-10-17 07:03:40 +0900

    header commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const humbergerMenu = document.querySelector('.humberger-menu');
+
+const navi = document.getElementById('humberger-navigation');
+
+const humbergerMenuSections = document.querySelectorAll('.humberger-menu-section');
+
+humbergerMenu.addEventListener('click', function () {
+  humbergerMenu.classList.toggle('active');
+  navi.classList.toggle('active');
+});
+
+humbergerMenuSections.forEach((humbergerMenuSection) => {
+  humbergerMenuSection.addEventListener('click', function () {
+    humbergerMenu.classList.remove('active');
+    navi.classList.remove('active');
+  });
+});
+
+console.log(toggleImg.classList.contains('active'));
